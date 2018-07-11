7f4b316ad1a08ba5da189252b3a2c39e885d3fa8
konstantinGulish <konstantin_gulish@yahoo.com>
2018-07-11 12:05:21 -0400

    changed enroll functionality

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,9 +1,15 @@
 package com.example.demo.controller;
 
+import com.example.demo.models.Major;
+import com.example.demo.models.Student;
 import com.example.demo.repository.AppRoleRepository;
 import com.example.demo.repository.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
